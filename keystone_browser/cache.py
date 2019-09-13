232fa9e3cc315b1afd9fa168918f66fb71ca8ace
Bryan Davis <bd808@wikimedia.org>
2019-09-12 23:38:25 -0600

    Use full service name for redis

--- keystone_browser/cache.py
+++ keystone_browser/cache.py
@@ -30,7 +30,7 @@ class Cache(object):
     def __init__(self, enabled=True, seed=''):
         self.enabled = enabled
         self.conn = redis.StrictRedis(
-            host='tools-redis',
+            host='tools-redis.svc.eqiad.wmflabs',
             decode_responses=True,
         )
         u = pwd.getpwuid(os.getuid())
